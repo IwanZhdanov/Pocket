700461e3cc614618883a55d0c3e7e17dffc1e4f0
Iwan <iwanzhdanov2007@gmail.com>
2021-06-21 08:42:27 +0300

    Второй коммит

--- pocket.py
+++ pocket.py
@@ -1,10 +1,31 @@
 from window_tk import Window
 from threading import Timer
 import os
+import re
+import time
 from random import randint as rand
 
 startdir = 'list/'
 ext = '.list'
+
+class Modes:
+    def __init__(self, items):
+        self._items = items
+        self._act = [0, 0]
+
+    def sw(self):
+        if self._act[1] < time.time():
+            self._items = [self._items[self._act[0]]] + self._items[0:self._act[0]] + self._items[self._act[0]+1:]
+            self._act[0] = 1
+        else:
+            self._act[0] = (self._act[0] + 1) % len(self._items)
+        self._act[1] = time.time() + 3
+        return self.get()
+    def get(self):
+        return self._items[self._act[0]][0]
+    def text(self):
+        return self._items[self._act[0]][1]
+
 
 class RecList:
     def __init__(self, name):
@@ -34,9 +55,20 @@ class RecList:
             self._data = self._data[0:1] + [value] + self._data[1:]
             self.save()
 
+    def change(self, old_txt, new_txt):
+        for i in range(len(self._data)):
+            if self._data[i] == old_txt:
+                self._data[i] = new_txt
+        self.save()
+
     def first(self, value):
         if value not in self._data:
             self._data = [value] + self._data
+            self.save()
+
+    def append(self, value):
+        if value not in self._data:
+            self._data.append(value)
             self.save()
 
     def info(self):
@@ -112,6 +144,47 @@ class AddForm:
         self.func(self.tk.read(self.name))
         self.tk.close()
 
+class RemarkForm:
+    def __init__(self, func):
+        self.func = func
+        w = Window('Записать')
+        self.tk = w
+        w.label('Заметка:')
+        w.nextCol(1, 400)
+        self.name = w.input()
+        w.nextRow()
+        w.button('OK')
+        w.onSubmit(self.submit)
+        w.nextCol()
+        w.button('Отмена')
+        w.onReset(w.close)
+        w.go()
+    def submit(self, tag):
+        self.func(self.tk.read(self.name))
+        self.tk.close()
+
+class RenameForm:
+    def __init__(self, name, func):
+        self.func = func
+        self.old_name = name
+        w = Window('Переименовать')
+        self.tk = w
+        w.label('Старое название:')
+        w.label('Новое название:')
+        w.nextCol()
+        w.label(name)
+        self.new_name = w.input()
+        w.nextRow()
+        w.button('OK')
+        w.onSubmit(self.submit)
+        w.nextCol()
+        w.button('Отмена')
+        w.onReset(w.close)
+        w.go()
+    def submit(self, tag):
+        self.func(self.old_name, self.tk.read(self.new_name))
+        self.tk.close()
+
 
 class MainForm:
     def __init__(self):
@@ -119,6 +192,8 @@ class MainForm:
         self.run = True
         self.lastInfo = ''
         self.lastMode = 'random'
+        self.mode = Modes([('link','Ссылки'),('off','Откл'),('nopassw','Норм'),('all','Всё')])
+        self.canon = Modes([('off','Нет'),('on','Да')])
 
         w = Window('Карман v2.0')
         self.tk = w
@@ -128,7 +203,11 @@ class MainForm:
         w.onClick(lambda x: AddForm(self.addList))
         w.nextRow(0,30)
         w.button('Переименовать')
-        w.nextRow(1, 400)
+        w.onClick(self.prep_rename)
+        w.nextRow(0,30)
+        w.button('Заметка')
+        w.onClick(lambda x: RemarkForm(self.addToList))
+        w.nextRow(1, 400) # FIXME:
         self.filelist = w.listbox()
         w.onSelect(self.showList)
         w.onDoubleClick(self.forceAdd)
@@ -138,45 +217,90 @@ class MainForm:
         w.endRow()
         w.nextCol(2, 300)
         w.startRow(0,30)
-        w.firstCol(1,100)
+        w.firstCol(1,150)
         w.button('Повтор')
         w.onClick(self.restore)
-        w.nextCol(1,100)
+        w.nextCol(1,150)
         w.button('Заменить')
         w.onClick(self.another)
-        w.nextCol(1,100)
+        w.nextCol(1,150)
         w.button('Сначала')
         w.onClick(self.asStart)
         w.nextRow(0,60)
-        w.firstCol(1,100)
+        w.firstCol(1,150)
         w.button('Стек')
         w.onClick(self.readAsStack)
-        w.nextCol(1,100)
+        w.nextCol(1,150)
         w.button('Рандом')
         w.onClick(self.readAsRandom)
-        w.nextCol(1,100)
+        w.nextCol(1,150)
         w.button('Очередь')
         w.onClick(self.readAsQueue)
-        w.nextRow()
+        w.nextRow() # FIXME:
         self.records = w.listbox()
+        w.onSelect(self.makeMenu)
         w.onDoubleClick(self.removeRecord)
+        w.nextRow(1,50)
+        self.variants = w.listbox()
+        w.onSelect(self.pickVariant)
         w.nextRow(0,30)
-        w.firstCol(1,100)
-        w.button('Кан.')
-        w.onClick(lambda x: AddForm(self.test))
-        w.nextCol(1,100)
-        w.button('Реж.')
-        w.nextCol(1,100)
+        w.firstCol(1,150)
+        self.can_btn = w.button('Кан.')
+        w.onClick(self.chCan)
+        w.nextCol(1,150)
+        self.mod_btn = w.button('Реж.')
+        w.onClick(self.chMod)
+        w.nextCol(1,150)
         w.button('Корзина')
+        w.onClick(lambda x: w.alert('','Не реализовано'))
         w.endRow()
+
         self.drawFileList()
         self.checkClipboard()
+        self.showStates()
         w.go()
         self.run = False
 
+    def is_passw(self, s):
+        if re.search('^[A-Za-z0-9]{10,20}$', s):
+            return True
+        return False
+
+    def make_canonical(self, txt):
+        sett = 'settings.dat'
+        ret = txt
+        if os.path.isfile(sett):
+            f = open (sett, 'r')
+            found = False
+            while True:
+                s = f.readline()
+                if not s: break
+                s = s[:-1]
+                if s[0] == '#':
+                    found = re.search(s[1:], txt)
+                elif found:
+                    cmd = re.search('([^:]*):(.*)', s)
+                    if not cmd[1]:
+                        mem = cmd[2]
+                        last = found.lastindex
+                        for i in range(0,last+1):
+                            mem = mem.replace('$'+str(i), found[i])
+                        ret = mem
+            f.close()
+        return ret
+
     def checkClipboard(self):
         info = self.getFromClipboard()
-        if info != self.lastInfo:
+        info = re.search('^[^\r\n]*', info)[0]
+        flag = True
+        if not info or info == self.lastInfo: flag = False
+        mod = self.mode.get()
+        if mod == 'off': flag = False
+        if mod == 'link' and info[0:4] != 'http': flag = False
+        if mod == 'nopassw' and self.is_passw(info): flag = False
+        if self.canon.get() == 'on': info = self.make_canonical(info)
+        if not info or info == self.lastInfo: flag = False
+        if flag:
             self.lastInfo = info
             self.addToList(info)
         if self.run: Timer(1, self.checkClipboard).start()
@@ -251,6 +375,7 @@ class MainForm:
         self.setToClipboard(val)
         self.lastInfo = val
         self.showList(None)
+        self.makeMenuByStr(val)
     def readAsStack(self, tag):
         self.readCurrentValue('stack')
     def readAsQueue(self, tag):
@@ -258,12 +383,32 @@ class MainForm:
     def readAsRandom(self, tag):
         self.readCurrentValue('random')
 
+    def rename(self, old_name, new_name):
+        x = RecList(old_name)
+        y = RecList(new_name)
+        if not y.info():
+            for i in x.info():
+                y.append(i)
+            x.delete()
+            self.currentName = new_name
+            files = self.getFileList()
+            for i in files:
+                z = RecList(i)
+                z.change(old_name, new_name)
+        self.drawFileList()
+    def prep_rename(self, tag):
+        name = self.getListName()
+        if name:
+            RenameForm(name, self.rename)
+
     def restore(self, tag):
         name = self.getListName()
         if name:
             x = RecList(name)
             M = x.info()
-            if M: self.setToClipboard(M[0])
+            if M:
+                self.setToClipboard(M[0])
+                self.makeMenuByStr(M[0])
 
     def markAsStart(self, name):
         x = RecList(name)
@@ -282,10 +427,24 @@ class MainForm:
             self.markAsStart(name)
         self.showList(None)
 
+    def markAsStart_soft(self, name):
+        x = RecList(name)
+        info = x.info()
+        if len(info) >= 2 and os.path.isfile(startdir+info[1]+ext):
+            self.markAsStart_soft(info[1])
+            x.get('stack')
+        if '--' not in x.info():
+            x.first('--')
+    def asStart_soft(self, tag):
+        name = self.getListName()
+        if name:
+            self.markAsStart_soft(name)
+        self.showList(None)
+
     def another(self, tag):
         if self.lastMode == 'stack':
             self.lastMode = 'random'
-        self.asStart(None)
+        self.asStart_soft(None)
         self.readCurrentValue(self.lastMode)
 
     def getFromClipboard(self):
@@ -298,6 +457,55 @@ class MainForm:
     def setToClipboard(self, data):
         self.tk.tk.clipboard_clear()
         self.tk.tk.clipboard_append(data)
+
+    def makeMenuOptions(self, txt):
+        sett = 'settings.dat'
+        ret = []
+        if os.path.isfile(sett):
+            f = open(sett, 'r')
+            cond = False
+            while True:
+                s = f.readline()
+                if not s: break
+                s = s[:-1]
+                if not s: continue
+                if s[0] == '#':
+                    cond = re.search(s[1:], txt)
+                elif cond:
+                    cmd = re.search('([^:]*):(.*)', s)
+                    if cmd[1]:
+                        opt = cmd[2]
+                        q = cond.lastindex
+                        for i in range(0,q+1):
+                            opt = opt.replace('$'+str(i), cond[i])
+                        ret.append(cmd[1]+': '+opt)
+            f.close()
+        return ret
+
+    def makeMenu(self, tag):
+        name = self.getListName()
+        options = self.tk.selection(self.records)
+        txt = ''
+        if name and options:
+            x = RecList(name)
+            n = options[0]
+            info = x.info()
+            if len(info) > n: txt = info[n]
+            self.tk.write(self.variants, self.makeMenuOptions(txt))
+
+    def makeMenuByStr(self, txt):
+        self.tk.write(self.variants, self.makeMenuOptions(txt))
+
+    def pickVariant(self, tag):
+        options = self.tk.selection(self.variants)
+        if options:
+            n = options[0]
+            vars = self.tk.read(self.variants)
+            line = vars[n]
+            a = line.index(': ')
+            txt = line[a+2:]
+            self.lastInfo = txt
+            self.setToClipboard(txt)
 
     def prep_removeList(self, tag):
         name = self.getListName()
@@ -312,5 +520,17 @@ class MainForm:
             self.currentName = ''
             self.tk.write(self.records, [])
             self.drawFileList()
+
+    def showStates(self):
+        self.tk.write(self.can_btn, 'К:'+self.canon.text())
+        self.tk.write(self.mod_btn, 'Р:'+self.mode.text())
+
+    def chCan(self, tag):
+        self.canon.sw()
+        self.showStates()
+
+    def chMod(self, tag):
+        self.mode.sw()
+        self.showStates()
 
 MainForm()
